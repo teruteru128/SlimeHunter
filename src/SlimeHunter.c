e89fc39e0cb12994fe5c369e016e6d8679691b48
Teruteru <teruterubouzu1024@gmail.com>
2019-10-29 00:07:49 +0900

    研究中
    新しいアルゴリズムを思いついて実装してみたけどメモリのアクセス効率が悪すぎる

--- src/SlimeHunter.c
+++ src/SlimeHunter.c
@@ -15,9 +15,11 @@ int64_t readSeed(void){
   }
   int64_t val = 0;
   size_t len = fread(&val, sizeof(val), 1, fp);
+  fclose(fp);
   return val;
 }
 
+void init_db_file(void);
 /*
    Minecraftのスライムチャンクのシード値探索ツール
    TODO 5x5向けに改修
@@ -28,5 +30,80 @@ int64_t readSeed(void){
    5チャンク連続でスライムチャンクな場所を見つけたら上下4チャンク、近い側から数える
 */
 int main(int argc, char* argv[]){
+  // DB file exists check
+  // get seed range list
+  // select seed range
+  //init_seed_db();
+  //seed_list list = NULL;
+  //get_seed_list(con, &list);
+  int64_t seed_start = 1613738097659009550L;
+  int64_t seed_end = 1613738097659009560L;
+
+  int32_t gx_min = -309;
+  int32_t gx_max = 312;
+  int32_t globalx = 0;
+  int32_t gy_min = -312;
+  int32_t gy_max = 309;
+  int32_t globaly = 0;
+
+  int32_t localx = 0;
+  const int32_t localx_range = 4;
+  int32_t localy = 0;
+  const int32_t localy_range = 4;
+
+
+  int64_t seed = 0;
+  SlimeChunkSeed seedt;
+  //foreach seedlist
+  int i=0;
+  for(seed = seed_start; seed <= seed_end; seed++){
+    setMCSeed(&seedt, seed);
+    for(globalx = gx_min; globalx <= gx_max; globalx += localx_range){
+      for(globaly = gy_min; globaly <= gy_max; globaly++){
+        for(localx = 0; isSlimeChunkXZ(&seedt, globalx+localx, globaly)&&localx < localx_range;localx++){
+        }
+
+        if(localx == localx_range){
+            //printf("%ld, %d, %d\n", seed, globalx*16, globaly*16);
+            //goto finish;
+          printf("5chunk seed,%ld,%d,%d\n", seed, globalx, globaly);
+          i=1;
+          for(localy = -1; localy > -localy_range;localy--){
+              if(isSlimeChunkXZ(&seedt, globalx+0, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+1, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+2, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+3, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+4, globaly+localy)){
+              i++;
+              printf("ue,%ld,%d,%d\n", seed, globalx, globaly);
+            }else{
+              break;
+            }
+          }
+          for(localy = 1; localy < localy_range;localy++){
+              if(isSlimeChunkXZ(&seedt, globalx+0, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+1, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+2, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+3, globaly+localy)
+              &&isSlimeChunkXZ(&seedt, globalx+4, globaly+localy)){
+              i++;
+              printf("shita,%ld,%d,%d\n", seed, globalx, globaly);
+            }else{
+              break;
+            }
+          }
+          if(i>=localy_range){
+             printf("found!, %ld,%d,%d\n", seed, globalx*16, globaly*16);
+          }
+        }else{
+          //globaly+=localx;
+        }
+      }
+    }
+  }
+  //mark_seed_range_as_searched
+  //end foreach seedlist
+finish:
+  return EXIT_SUCCESS;
 }
 
---
+++ src/SlimeHunter2.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
+#include <string.h>
+#include "random.h"
+#include "mcSlimeChunkOracle.h"
+
+int main(int argc, char* argv[]){
+
+  char chunks[1000]={};
+  int64_t seed, seed_min=-9158047567848165954LL, seed_max=seed_min+1000LL;
+  int32_t x,z,offsetx=-500, offsetz=-500;
+  SlimeChunkSeed seedt;
+  for(seed = seed_min; seed < seed_max; seed++){
+    setMCSeed(&seedt, seed);
+    //キャッシュゼロクリア
+    memset(chunks, 0, 1000);
+    //西の端5チャンク分をchunksに入れる
+    size_t filled_with_slime_chunks=0;
+    for(x=0;x<1000;x++){
+      filled_with_slime_chunks = 0;
+    //紅の豚の冒頭のように複数行まとめてキャッシュに入れる
+      for(z=0;z<1000;z++){
+          chunks[z] = (chunks[z]<<1 | isSlimeChunkXZ(&seedt,x+offsetx,z+offsetz)) & 0x1f;
+          //0x1fが連続しているかチェック
+          if(chunks[z] == 0x1f){
+            //printf("ヌッ %ld, %d, %d\n", seed, (x+offsetx)*16, (z+offsetz)*16);
+            filled_with_slime_chunks++;
+          }else{
+            filled_with_slime_chunks = 0;
+          }
+          if(filled_with_slime_chunks == 5){
+            printf("found! %ld, %d, %d\n", seed, x*16, z*16);
+          }
+        }
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
