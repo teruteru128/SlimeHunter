fb09c6166804874414de586319b3fa5d36bbfca3
Yuta Onose <onose999999999@gmail.com>
2019-12-17 09:54:03 +0900

    記録に残す

--- src/mcSlimeChunkOracle.c
+++ src/mcSlimeChunkOracle.c
@@ -7,6 +7,11 @@ void setMCSeed(SlimeChunkSeed* t, int64_t seed){
 }
 
 /*
+int64_t getMCSeed(int64_t mc_seed, int32_t chunkX, int32_t chunkZ){
+  return mc_seed + chunkX * chunkX * 0x4c1906 + chunkX * 0x5ac0db + chunkZ * chunkZ * 0x4307a7L + chunkZ * 0x5f24f ^ 0x3ad8025f;
+}
+*/
+/*
    https://minecraft.gamepedia.com/Slime#.22Slime_chunks.22
    シード計算の各項は再利用できる
    chunkX*chunkX*0x4c1906
@@ -19,6 +24,11 @@ int64_t getMCSeed(SlimeChunkSeed* seed, int32_t chunkX, int32_t chunkZ){
   return seed->seed + chunkX * chunkX * 0x4c1906 + chunkX * 0x5ac0db + chunkZ * chunkZ * 0x4307a7L + chunkZ * 0x5f24f ^ 0x3ad8025f;
 }
 
+/*
+   bool isSlimeChunk(int64_t seed, int32_t x_chunk, int32_t z_chunk){
+      return nextIntWithRange(getMCSeed(seed, x_chunk, z_chunk));
+   }
+ */
 /*
    TODO: seed, x, zを引数に取るハッシュ関数として実装し直す
    bool isSlimeChunk(int64_t seed, int32_t x, int32_t z)
